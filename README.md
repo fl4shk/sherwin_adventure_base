f7e96a49e20694cc5fd28108fe78a22fb8bd782a
fl4shk <fl4shk@users.noreply.github.com>
2020-12-05 13:22:56 -0600

    new GNUmakefile, update README

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# Sherwin's Adventure
-This is my complete redo of Sherwin's Adventure, this time not for the GBA.
-At first, it's going to use SFML for the main graphics, audio, and input
-facilities, but there are plans to, if I get far enough in the project, to
-support 3D graphics as well.
+# Sherwin's Adventure Base
+This is the base game engine for my video game, *Sherwin's Adventure*.
 
+Some of the assets that I deemed useful for modding the game have been
+included as well.
 
-## Reasoning for No More GBA Version
-I decided to make a small game with about 10 levels for the GBA instead.
--- src/misc_includes.hpp
+++ src/misc_includes.hpp
@@ -1,22 +1,38 @@
-#ifndef misc_includes_hpp
-#define misc_includes_hpp
+#ifndef src_misc_includes_hpp
+#define src_misc_includes_hpp
 
+// src/misc_includes.hpp
 
+// This file is part of Sherwin's Adventure Base.
+// 
+// Sherwin's Adventure Base is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// Sherwin's Adventure Base is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with Sherwin's Adventure Base.  If not, see
+// <https://www.gnu.org/licenses/>.
 
+#include "liborangepower_src/misc/misc_includes.hpp"
+#include "liborangepower_src/misc/misc_output_classes.hpp"
+#include "liborangepower_src/misc/misc_types.hpp"
+#include "liborangepower_src/misc/misc_utility_funcs.hpp"
+#include "liborangepower_src/misc/misc_bitwise_funcs.hpp"
+#include "liborangepower_src/misc/misc_defines.hpp"
+#include "liborangepower_src/gen_class_innards_defines.hpp"
+#include "liborangepower_src/strings/string_conversion_stuff.hpp"
 
-#include "liborangepower_src/misc_includes.hpp"
-#include "liborangepower_src/misc_output_classes.hpp"
-#include "liborangepower_src/misc_types.hpp"
-#include "liborangepower_src/misc_utility_funcs.hpp"
-#include "liborangepower_src/misc_bitwise_funcs.hpp"
-#include "liborangepower_src/misc_defines.hpp"
-#include "liborangepower_src/gen_getter_setter_defines.hpp"
-
-using namespace liborangepower::printout_etc;
+using namespace liborangepower::misc_output;
 using namespace liborangepower::integer_types;
 using namespace liborangepower::misc_util;
 using namespace liborangepower::bitwise;
+using namespace liborangepower::strings;
 
 
-
-#endif		// misc_includes_hpp
+#endif		// src_misc_includes_hpp
